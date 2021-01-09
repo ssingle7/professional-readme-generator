91a027f6fd02e71dfb90463c7344876e12777c2a
Selena Singleton <ssingle7@gmail.com>
2021-01-08 22:07:50 -0500

    added the remaining questions for the initialize app after testing to ensure no errors occurred

--- index.js
+++ index.js
@@ -14,6 +14,48 @@ const questions = [
         message: "What is your email address?",
         name: "email",
     },
+    
+    {
+        type: "input", 
+        message: "What is your project's name?",
+        name: "project",
+    },
+    
+    {
+        type: "input", 
+        message: "Please write a short description of your project",
+        name: "description",
+    },
+    
+    {
+        type: "input", 
+        message: "What kind of license should your project have?",
+        name: "license",
+    },
+
+    {
+        type: "input", 
+        message: "What command should be run to install dependencies?",
+        name: "install",
+    },
+    
+    {
+        type: "input", 
+        message: "What command should be run to run tests?",
+        name: "test",
+    },
+
+    {
+        type: "input", 
+        message: "What does the user need to know about using the repo?",
+        name: "repo",
+    },
+
+    {
+        type: "input", 
+        message: "What does the user need to know about contributing to the repo?",
+        name: "contributors",
+    },
 ];
 
 // TODO: Create a function to write README file
