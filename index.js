7a05973a5445b5b7d9866605afe0a3bae7be0ca4
Selena Singleton <ssingle7@gmail.com>
2021-01-08 21:08:31 -0500

    added const variables and questions array for user prompt

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 // TODO: Include packages needed for this application
-
+const fs = required("fs"); 
+const inquirer = required("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input", 
+        message: "What is your Github username?",
+        name: "name"
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("The function has run");
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+    });
+}
 
 // Function call to initialize app
 init();
