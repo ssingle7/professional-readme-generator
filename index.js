28011a3332033ac6da80981920bcfc90ce5974e8
Selena Singleton <ssingle7@gmail.com>
2021-01-08 21:44:59 -0500

    corrected grammatical errors in file index.js to initialize app

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 // TODO: Include packages needed for this application
-const fs = required("fs"); 
-const inquirer = required("inquirer");
+const fs = require("fs"); 
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
 const questions = [
     {
         type: "input", 
         message: "What is your Github username?",
-        name: "name"
-    }
+        name: "name",
+    },
+    
+    {
+        type: "input", 
+        message: "What is your email address?",
+        name: "email",
+    },
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
