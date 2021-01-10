c516404ca9e065da081638dda401adf934318eba
Selena Singleton <ssingle7@gmail.com>
2021-01-09 22:41:46 -0500

    corrected the indention in the index.js file to display the README correctly

--- generatedMarkdown.md
+++ generatedMarkdown.md
@@ -1,32 +1,32 @@
-# lalalala
-        https://github.com/lalala
-        *E-mail: lalalala
-        #  Description
-        lalalala
-        ##  Table of Contents
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [License](#license)
-      * [Contributing](#contributing)
-      * [Tests](#tests)
-      * [Questions](#questions)
-      # Installation
-      * The following necessary dependencies must be installed to run the application 
-      lalalalala
-      #  Usage
-      *In order to use this app, 
-      lalalal
-      #  License
-      This project is licensed under the lalalalal license. 
-      ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
-      #  Contributing
-      Contributors: 
-      lalalaal
-      # Tests 
-      The following is needed to run the test: 
-      undefined
-      # Questions
-      If you have any questions about the repo, open an issue or contact 
-      
-        
-      
+# 111
+https://github.com/111
+*E-mail: 111
+#  Description
+111
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+# Installation
+* The following necessary dependencies must be installed to run the application 
+111
+# Usage
+*In order to use this app, 
+111
+# License
+This project is licensed under the 111 license. 
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+#  Contributing
+Contributors: 
+111
+# Tests 
+The following is needed to run the test: 
+undefined
+# Questions
+If you have any questions about the repo, open an issue or contact 
+    
+    
+    
--- index.js
+++ index.js
@@ -66,37 +66,37 @@ const questions = [
 function generateMarkdown(data) {
     {
         return `# ${data.title}
-        https://github.com/${data.username}
-        *E-mail: ${data.email}
-        #  Description
-        ${data.description}
-        ##  Table of Contents
-      * [Installation](#installation)
-      * [Usage](#usage)
-      * [License](#license)
-      * [Contributing](#contributing)
-      * [Tests](#tests)
-      * [Questions](#questions)
-      # Installation
-      * The following necessary dependencies must be installed to run the application 
-      ${data.install}
-      #  Usage
-      *In order to use this app, 
-      ${data.usage}
-      #  License
-      This project is licensed under the ${data.license} license. 
-      ![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
-      #  Contributing
-      Contributors: 
-      ${data.contributors}
-      # Tests 
-      The following is needed to run the test: 
-      ${data.tests}
-      # Questions
-      If you have any questions about the repo, open an issue or contact 
-      
-        
-      `;
+https://github.com/${data.username}
+*E-mail: ${data.email}
+#  Description
+${data.description}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+# Installation
+* The following necessary dependencies must be installed to run the application 
+${data.install}
+# Usage
+*In order to use this app, 
+${data.usage}
+# License
+This project is licensed under the ${data.license} license. 
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+#  Contributing
+Contributors: 
+${data.contributors}
+# Tests 
+The following is needed to run the test: 
+${data.tests}
+# Questions
+If you have any questions about the repo, open an issue or contact 
+    
+    
+    `;
       }
 }
 
